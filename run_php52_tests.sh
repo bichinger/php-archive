53262fd36cf181bd46cda9a9bd99731a73e6e495
Roman Wollenburg <r.wollenburg@bichinger.de>
2017-03-01 09:16:34 +0100

    check for vendor folder added

--- run_php52_tests.sh
+++ run_php52_tests.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
+# check if vendor files exists
+if [ ! -d $"{PWD}/vendor" ]; then
+    composer install
+fi
+
+
 # cleanup old containers
+echo ""
+echo "> Cleanup old testcontainer..."
 docker rm -f php-archive-tests &> /dev/null
 # start test containers
+echo ""
+echo "> Starting testcontainer..."
 docker run -d --name php-archive-tests -p8888:80 -v ${PWD}:/var/www/app/html  yappabe/apache-php:5.2
 # execute t+phpunit tests
+echo ""
+echo "> Excuting tests... "
 docker exec -it php-archive-tests /var/www/app/html/tests/run_phpunit.sh
 # kill test container
+echo ""
+echo "> Remove testcontainer..."
 docker rm -f php-archive-tests &> /dev/null
