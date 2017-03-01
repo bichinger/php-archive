b95dc990dd4acfb0da124c56380b95b02ad67683
Roman Wollenburg <r.wollenburg@bichinger.de>
2017-03-01 08:22:44 +0100

    autoload file for tests added

---
+++ tests/autoload_52.php
@@ -0,0 +1,6 @@
+<?php
+    require(dirname(__FILE__)."/../vendor/autoload.php");
+    require(dirname(__FILE__)."/../src/Archive.php");
+    require(dirname(__FILE__)."/../src/FileInfo.php");
+    require(dirname(__FILE__)."/../src/Tar.php");
+    require(dirname(__FILE__)."/../src/Zip.php");
