e7d98cdfe26dff4f4dfd161672540bd1c6760382
Roman Wollenburg <r.wollenburg@bichinger.de>
2017-03-01 09:08:23 +0100

    custom php 5.2 autload file added to work with phpunit-php52

--- tests/autoload_52.php
+++ tests/autoload_52.php
@@ -1,5 +1,17 @@
 <?php
-    require(dirname(__FILE__)."/../vendor/autoload.php");
+
+    function addIncludePath($path){
+        set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+    }
+    addIncludePath(dirname(__FILE__).'/../vendor/phpunit/php-file-iterator');
+    addIncludePath(dirname(__FILE__).'/../vendor/phpunit/php-code-coverage');
+    addIncludePath(dirname(__FILE__).'/../vendor/phpunit/php-timer');
+    addIncludePath(dirname(__FILE__).'/../vendor/phpunit/php-text-template');
+    addIncludePath(dirname(__FILE__).'/../vendor/phpunit/php-token-stream');
+    addIncludePath(dirname(__FILE__).'/../vendor/phpunit/phpunit-mock-objects-php52');
+    addIncludePath(dirname(__FILE__).'/../vendor/phpunit/phpunit-php52');
+
+    require(dirname(__FILE__)."/../vendor/phpunit/phpunit-php52/PHPUnit/Autoload.php");
     require(dirname(__FILE__)."/../src/Archive.php");
     require(dirname(__FILE__)."/../src/FileInfo.php");
     require(dirname(__FILE__)."/../src/Tar.php");
